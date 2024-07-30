91500ed08ddcbb2fd6ab50151fe7d0efebd1a7ba
Antonia Soler <asoler2004@gmail.com>
2024-07-30 18:26:58 -0300

    Update UserInterface.py

--- User Interface/UserInterface.py
+++ User Interface/UserInterface.py
@@ -96,30 +96,22 @@ admission_source_id_mapping = {
 
 
 # diccionario de valores limite
-diccionario = {'upper_limit_admission_type_id': 6.0,
- 'lower_limit_admission_type_id': -2.0,
- 'upper_limit_discharge_disposition_id': 8.5,
- 'lower_limit_discharge_disposition_id': -3.5,
- 'upper_limit_admission_source_id': 16.0,
- 'lower_limit_admission_source_id': -8.0,
- 'upper_limit_time_in_hospital': 12.0,
- 'lower_limit_time_in_hospital': -4.0,
- 'upper_limit_num_lab_procedures': 96.0,
- 'lower_limit_num_lab_procedures': -8.0,
- 'upper_limit_num_procedures': 5.0,
- 'lower_limit_num_procedures': -3.0,
- 'upper_limit_num_medications': 33.5,
- 'lower_limit_num_medications': -2.5,
- 'upper_limit_number_outpatient': 0.0,
- 'lower_limit_number_outpatient': 0.0,
- 'upper_limit_number_emergency': 0.0,
- 'lower_limit_number_emergency': 0.0,
- 'upper_limit_number_inpatient': 2.5,
- 'lower_limit_number_inpatient': -1.5,
- 'upper_limit_number_diagnoses': 13.5,
- 'lower_limit_number_diagnoses': 1.5}
 
-global_result = ""
+diccionario = {'upper_limit_admission_type_id': 3.0,
+ 'lower_limit_admission_type_id': 1.0,
+ 'upper_limit_discharge_disposition_id': 4,
+ 'lower_limit_discharge_disposition_id': 1,
+ 'upper_limit_admission_source_id': 7.0,
+ 'lower_limit_admission_source_id': 1.0,
+ 'upper_limit_time_in_hospital': 6.0,
+ 'lower_limit_time_in_hospital': 2.0,
+ 'upper_limit_num_lab_procedures': 57.0,
+ 'lower_limit_num_lab_procedures': 31.0,
+ 'upper_limit_num_procedures': 2.0,
+ 'lower_limit_num_procedures': 0.0,
+ 'upper_limit_num_medications': 20.0,
+ 'lower_limit_num_medications': 11.0,
+ }
 
 def user_input_features(admission_source_id,
                         admission_type_id,
@@ -128,9 +120,7 @@ def user_input_features(admission_source_id,
                         num_lab_procedures,
                         num_procedures,
                         num_medications,
-                        number_outpatient,
-                        number_emergency,
-                        number_inpatient):
+                         ):
 
 
   atidv = list(admission_type_id_mapping.values())
@@ -146,22 +136,18 @@ def user_input_features(admission_source_id,
   data3 = ddidk[ddidv.index(discharge_disposition_id)]
 
 
-
-  data=[data1,
-        data3,
-        data2,
-        time_in_hospital,
-        num_lab_procedures,
-        num_procedures,
-        num_medications,
-        number_outpatient,
-        number_emergency,
-        number_inpatient ]
+  data=[(data1-diccionario['lower_limit_admission_type_id'])/(diccionario['upper_limit_admission_type_id']-diccionario['lower_limit_admission_type_id']),
+        (data3-diccionario['lower_limit_discharge_disposition_id'])/(diccionario['upper_limit_discharge_disposition_id']-diccionario['lower_limit_discharge_disposition_id']),
+        (data2-diccionario['lower_limit_admission_source_id'])/(diccionario['upper_limit_admission_source_id']-diccionario['lower_limit_admission_source_id']),
+        (time_in_hospital-diccionario['lower_limit_time_in_hospital'])/(diccionario['upper_limit_time_in_hospital']-diccionario['lower_limit_time_in_hospital']),
+        (num_lab_procedures-diccionario['lower_limit_num_lab_procedures'])/(diccionario['upper_limit_num_lab_procedures']-diccionario['lower_limit_num_lab_procedures']),
+        (num_procedures-diccionario['lower_limit_num_procedures'])/(diccionario['upper_limit_num_procedures']-diccionario['lower_limit_num_procedures']),
+        (num_medications-diccionario['lower_limit_num_medications'])/(diccionario['upper_limit_num_medications']-diccionario['lower_limit_num_medications'])]
 
 
   df = pd.DataFrame(data).T
-  df.columns =x_lr_test.columns
-
+  df.columns = ['admission_type_id', 'discharge_disposition_id', 'admission_source_id',
+       'time_in_hospital', 'num_lab_procedures', 'num_procedures','num_medications']
 
   current_prediction = loaded_model.predict(df)
   if current_prediction[0]==0:
@@ -171,30 +157,18 @@ def user_input_features(admission_source_id,
     result = " HIGH "
     result_SP= " ALTA "
 
-  #text_to_display=  "The patient was admitted as : "+admission_type_id+"\n"
-  #text_to_display+= "The patient was admitted from : "+admission_source_id+"\n"
-  #text_to_display+= "The patient was discharged as : "+discharge_disposition_id+"\n"
-  #text_to_display+= "The patient spent : "+str(time_in_hospital)+" days in the hospital\n"
-  #text_to_display+= "The patient had : "+str(num_lab_procedures)+" lab procedures\n"
-  #text_to_display+= "            and "+str(num_procedures)+" other procedures\n"
-  #text_to_display+= "            and received "+str(num_medications)+" different medications\n"
-  #text_to_display+= "            during the stay in the hospital.\n"
-  #text_to_display+="The patient had "+str(number_outpatient)+" outpatient visits in the last year\n"
-  #text_to_display+="The patient had "+str(number_emergency)+" emergency visits in the last year\n"
-  #text_to_display+="The patient had "+str(number_inpatient)+" inpatient visits in the last year.\n\n"
   text_to_display="The model's prediction is that this patient's probability \n"
   text_to_display+="of being readmitted in the hospital in the next 30 days is \n"
   text_to_display+="*********************"+result+"************************"
-  #st.popover("Model executed. Switch to next tab to see the model's Prediction")
-  #with tab4:
-  st.write(text_to_display)
+  st.popover(text_to_display)
   return text_to_display
 
-tab1,tab2,tab3,tab4 = st.tabs(['Main', 'About HospRead', 'Data Input', 'Model Prediction' ])
+tab1,tab2,tab3,tab4 = st.tabs(['Main', 'About HospRead', 'Data Input', 'Model Status' ])
 with tab1:
    
 
   image_url='https://github.com/asoler2004/HospRead/raw/main/LogoHospRead.jpg'
+  #image_url='https://github.com/No-Country-simulation/c19-109-m-data-bi/raw/main/Images/Logo_HospRead.jpg'
   response = requests.get(image_url)
 
   # Check if the request was successful
@@ -206,7 +180,22 @@ with tab1:
     st.write("Failed to download image. Status code:", response.status_code)
     
   
+with tab4:
+  #path='https://github.com/No-Country-simulation/c19-109-m-data-bi/raw/main/ML_training/hospread_logistic_regression.pkl'
+  #path='https://github.com/asoler2004/nocountryc19/raw/main/hospread_logistic_regression.pickle'
+  path='https://github.com/asoler2004/nocountryc19/raw/main/hospread_logistic_regression.pkl'
+  response = requests.get(path)
+
+  # Check if the request was successful
+  if response.status_code == 200:
+    # Load the model using pickle
+    loaded_model = pickle.loads(response.content)
+  else:
+    st.write("Failed to download the model. Status code:", response.status_code)
   
+  st.write(loaded_model)
+
+   
 with tab2:
   st.write("Flor Jazmin Cisneros")
   st.write("Juan Espinosa")
@@ -220,83 +209,14 @@ with tab3:
   num_lab_procedures= st.slider('NUMBER OF LAB PROCEDURES',diccionario['lower_limit_num_lab_procedures'], diccionario['upper_limit_num_lab_procedures'], value=diccionario['lower_limit_num_lab_procedures'])
   num_procedures=st.slider('NUMBER OF OTHER PROCEDURES',diccionario['lower_limit_num_procedures'], diccionario['upper_limit_num_procedures'], value=diccionario['lower_limit_num_procedures'])
   num_medications= st.slider('NUMBER OF MEDICATIONS',diccionario['lower_limit_num_medications'], diccionario['upper_limit_num_medications'], value=diccionario['lower_limit_num_medications'])
-  number_outpatient= st.slider('NUMBER OF OUTPATIENT VISITS IN LAST YEAR',diccionario['lower_limit_number_outpatient'], diccionario['upper_limit_number_outpatient']+100, value=diccionario['lower_limit_number_outpatient'])
-  number_emergency = st.slider('NUMBER OF EMERGENCY VISITS IN LAST YEAR',diccionario['lower_limit_number_emergency'], diccionario['upper_limit_number_emergency']+100, value=diccionario['lower_limit_number_emergency'])
-  number_inpatient= st.slider('NUMBER OF INPATIENT VISITS IN LAST YEAR',diccionario['lower_limit_number_inpatient'], diccionario['upper_limit_number_inpatient'], value=diccionario['lower_limit_number_inpatient'])
   st.button("Submit",on_click= user_input_features,args=[admission_source_id,
                                                          admission_type_id,
                                                          discharge_disposition_id,
                                                          time_in_hospital,
                                                          num_lab_procedures,
                                                          num_procedures,
-                                                         num_medications,
-                                                         number_outpatient,
-                                                         number_emergency,
-                                                         number_inpatient])
+                                                         num_medications
+                                                         ])
                                                             
 
 
-
-with tab4:
-  path='https://github.com/asoler2004/nocountryc19/raw/main/hospread_logistic_regression.pickle'
-  response = requests.get(path)
-
-  # Check if the request was successful
-  if response.status_code == 200:
-    # Load the model using pickle
-    loaded_model = pickle.loads(response.content)
-    #st.write("Model loaded successfully!")
-  else:
-    st.write("Failed to download the model. Status code:", response.status_code)
-  
-  st.write(loaded_model)
-  # load datasets
-  path='https://github.com/asoler2004/nocountryc19/raw/main/X_LR_SMOTE.pickle'
-  response = requests.get(path)
-
-  # Check if the request was successful
-  if response.status_code == 200:
-    # Load the data using pickle
-    x_lr_smote = pickle.loads(response.content)
-    st.write("X loaded successfully!")
-    #st.write(x_lr_smote.head())
-  else:
-    st.write("Failed to download the data. Status code:", response.status_code)
-
-  path='https://github.com/asoler2004/nocountryc19/raw/main/Y_LR_SMOTE.pickle'
-  response = requests.get(path)
-
-  # Check if the request was successful
-  if response.status_code == 200:
-    # Load the data using pickle
-    y_lr_smote = pickle.loads(response.content)
-    st.write("Y loaded successfully!")
-    #st.write(y_lr_smote.head())
-  else:
-    st.write("Failed to download the data. Status code:", response.status_code)
-
-  path='https://github.com/asoler2004/nocountryc19/raw/main/X_LR_TEST.pickle'
-  response = requests.get(path)
-
-  # Check if the request was successful
-  if response.status_code == 200:
-    # Load the data using pickle
-    x_lr_test = pickle.loads(response.content)
-    st.write("X test loaded successfully!")
-    #st.write(x_lr_test.head())
-  else:
-    st.write("Failed to download the data. Status code:", response.status_code)
-
-  path='https://github.com/asoler2004/nocountryc19/raw/main/Y_LR_TEST.pickle'
-  response = requests.get(path)
-
-  # Check if the request was successful
-  if response.status_code == 200:
-    # Load the data using pickle
-    y_lr_test = pickle.loads(response.content)
-    st.write("Y test loaded successfully!")
-    #st.write(y_lr_test.head())
-  else:
-    st.write("Failed to download the data. Status code:", response.status_code)
-
- 
