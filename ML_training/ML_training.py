0c9b4c8dabe1c910e6ffb90bff81fb7d29ddd86c
Santiago <104315806+Smsantimaldonado@users.noreply.github.com>
2024-07-30 15:11:06 -0300

    ML training update

--- ML_training/ML_training.py
+++ ML_training/ML_training.py
@@ -14,7 +14,7 @@ from sklearn.linear_model import LogisticRegression
 import pickle
 
 # Acceder al archivo
-access_token = 'ghp_xxxxxxxxx' # Acá deben reemplazar por su token de GitHub
+access_token = '' # Acá deben reemplazar por su token de GitHub
 usuario = 'No-Country-simulation'
 repositorio = 'c19-109-m-data-bi'
 
@@ -39,37 +39,37 @@ final_df['encounter_id'] = final_df['encounter_id'].astype(str)
 final_df['patient_nbr'] = final_df['patient_nbr'].astype(str)
 ids_features = list(final_df.select_dtypes(include=['object']).columns)
 
-
 # Selección de variables
 sfs = SFS(LogisticRegression(max_iter=10000), k_features=8, forward=True, floating=False, scoring = 'accuracy', cv = 5)
 target_variable = 'readmitted'
 
-X_lr = final_df.drop(target_variable, axis=1)
-X_lr.drop(columns=ids_features, inplace=True)
-y_lr = final_df[target_variable]
+X = final_df.drop(target_variable, axis=1)
+X.drop(columns=ids_features, inplace=True)
+y = final_df[target_variable]
 
-sfs.fit(X_lr, y_lr)
+print('pre selección de variables')
+sfs.fit(X, y)
 variables_elegidas = list(sfs.k_feature_names_)
+print(variables_elegidas)
 
 #Train/test
-X_lr = final_df[variables_elegidas] #sobreescribiendo la variable X_lr
-# y_lr ya había sido definida unas celdas antes
+X = final_df[variables_elegidas] #sobreescribiendo la variable X
+# y ya había sido definida unas celdas antes
+print(X.columns)
 
 from sklearn.model_selection import train_test_split
-X_lr_train, X_lr_test, y_lr_train, y_lr_test = train_test_split(X_lr, y_lr, test_size = 1/3,
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3,
                                                                 random_state = 123, shuffle=True,
-                                                                stratify=y_lr)
+                                                                stratify=y)
 
 # Balanceo SMOTE
 from imblearn.over_sampling import SMOTE
 smote = SMOTE(sampling_strategy = .5)
-X_lr_resampled_smote, y_lr_resampled_smote = smote.fit_resample(X_lr_train, y_lr_train)
-
+X_resampled_smote, y_resampled_smote = smote.fit_resample(X_train, y_train)
 
 # Entrenamiento del modelo
 model_lr_smote = LogisticRegression(max_iter=10000)
-model_lr_smote.fit(X_lr_resampled_smote, y_lr_resampled_smote)
-
+model_lr_smote.fit(X_resampled_smote, y_resampled_smote)
 
 # Almacenamiento del modelo
 with open('hospread_logistic_regression.pkl', 'wb') as file:
